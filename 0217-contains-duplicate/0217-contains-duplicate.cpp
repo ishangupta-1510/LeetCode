b29037fe7b68cdcc00800bd1d2ab01ec1f377e8e
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-07-05 15:32:17 +0530

    Time: 197 ms (20.21%), Space: 70.8 MB (30.77%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            if(m[nums[i]] == 1){
+                return true;
+            }
+            m[nums[i]] = 1;
+        }
+        return false;
+    }
+};
