93f04899bc5f3ca364c87d43c7138c392009c5db
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-07-21 13:30:10 +0530

    Time: 339 ms (5.01%), Space: 56 MB (5.17%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    static bool comp(vector<int> a,vector<int> b){
+        if(a[0]<b[0]){
+            return true;
+        }
+        if(a[0] == b[0]){
+            return a[1]<b[1];
+        }
+        return false;
+    }
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end(),comp);
+        int i=0;
+        int n = intervals.size();
+        vector<vector<int>> v;
+        if(n == 1){
+            v.push_back(intervals[0]);
+        }
+        while(i<n-1){
+            int j = i+1;
+            while(j<n){
+                if(intervals[i][1]>=intervals[j][0]){
+                    intervals[i][1] = max(intervals[i][1],intervals[j][1]);
+                }
+                else{
+                    break;
+                }
+                j++;
+            }
+            v.push_back(intervals[i]);
+            i=j;
+        }
+        if(intervals[n-1][1] > v[v.size()-1][1]){
+            v.push_back(intervals[n-1]);
+        }
+        return v;
+    }
+};
