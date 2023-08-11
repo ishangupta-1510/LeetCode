d5e2b68790467d623ec2561bf01286a64f60db6b
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-08-11 14:22:03 +0530

    Time: 4 ms (78.74%) | Memory: 6.1 MB (96.82%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -3,31 +3,24 @@ public:
     int countSubstrings(string s) {
         int n = s.length();
         int ans = 0;
-
-        // Odd length palindromes
-        for (int i = 0; i < n; i++) {
-            int right = i, left = i;
+        for(int i=0;i<n;i++){
+            int right = i,left = i;
             int count = 0;
-            while (left >= 0 && right < n && s[left] == s[right]) {
+            while(left>=0 and s[left] == s[right] and right<n){
                 count++;
-                left--;
-                right++;
+                left--;right++;
             }
             ans += count;
         }
-
-        // Even length palindromes
-        for (int i = 0; i < n - 1; i++) {
-            int right = i + 1, left = i;
+        for(int i=0;i<n-1;i++){
+            int right = i+1,left = i;
             int count = 0;
-            while (left >= 0 && right < n && s[left] == s[right]) {
+            while(left>=0 and s[left] == s[right] and right<n){
                 count++;
-                left--;
-                right++;
+                left--;right++;
             }
             ans += count;
         }
-
         return ans;
     }
 };
