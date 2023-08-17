0ab8385a8e3e63353ab384995def5f9478674eb6
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-08-17 16:25:19 +0530

    Time: 298 ms (91.35%), Space: 8 MB (40.33%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    bool wordsearch(vector<vector<char>>& board, const string& word, int i, int j, int k, int n, int m) {
-        if (k >= word.length()) {
+    bool wordsearch(vector<vector<char>>& board, string& word,int i,int j,int k,int& n,int& m){
+        if(k>=word.length()){
             return true;
         }
-        if (i < 0 || j < 0 || i >= n || j >= m || board[i][j] != word[k]) {
+        if(i >= n or j>=m or i<0 or j<0 or board[i][j] != word[k]){
             return false;
         }
-        char temp = board[i][j]; // Store the current character
-        board[i][j] = '#'; // Mark the cell as visited
-        bool found = wordsearch(board, word, i + 1, j, k + 1, n, m) ||
-                     wordsearch(board, word, i, j + 1, k + 1, n, m) ||
-                     wordsearch(board, word, i - 1, j, k + 1, n, m) ||
-                     wordsearch(board, word, i, j - 1, k + 1, n, m);
-        board[i][j] = temp; // Restore the cell to its original state
+        char temp = board[i][j];
+        board[i][j] = '#';
+        bool  found = wordsearch(board,word,i+1,j,k+1,n,m) or wordsearch(board,word,i,j+1,k+1,n,m) or wordsearch(board,word,i-1,j,k+1,n,m) or wordsearch(board,word,i,j-1,k+1,n,m);
+        board[i][j] = temp;
         return found;
     }
-    
-    bool exist(vector<vector<char>>& board, const string& word) {
-        int n = board.size(), m = board[0].size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(),m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if (board[i][j] == word[0] && wordsearch(board, word, i, j, 0, n, m)) {
-                    return true;
-                }
+                    return true;                }
             }
         }
         return false;
