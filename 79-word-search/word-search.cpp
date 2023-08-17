8254a14d9bcca83c0dc07b4264228122f5920918
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-08-17 16:26:37 +0530

    Time: 296 ms (91.50%) | Memory: 8.1 MB (40.33%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -17,8 +17,9 @@ public:
         int n = board.size(),m=board[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if (board[i][j] == word[0] && wordsearch(board, word, i, j, 0, n, m)) {
-                    return true;                }
+                if(board[i][j] == word[0] and wordsearch(board,word,i,j,0,n,m)){
+                    return true;
+                }
             }
         }
         return false;
