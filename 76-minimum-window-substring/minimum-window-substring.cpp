3a0161920a44fbc8dca90d77015ef48c0e063642
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-08-23 15:20:58 +0530

    Time: 11 ms (82.85%) | Memory: 8 MB (56.40%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.length(), m = t.length();
-        vector<int> freq(128, 0);
-        for (char c : t) {
-            freq[c]++;
+        int n=s.length(),m=t.length();
+        vector<int> freq(128,0);
+        for(int i=0;i<m;i++){
+            freq[t[i]]++;
         }
-        int start = 0, st = 0, e = 0;
-        vector<int> freq1(128, 0);
-        int requiredChars = t.length(), minWindowSize = INT_MAX;
-
-        for (int i = 0; i < n; i++) {
-            if (freq[s[i]]) {
-                if (freq1[s[i]] < freq[s[i]]) {
-                    requiredChars--;
+        int start = 0,len = INT_MAX,required=m,minstart=0;
+        for(int end=0;end<n;end++){
+            if(freq[s[end]] > 0){
+                required--;
+            }
+            freq[s[end]]--;
+            while(required == 0){
+                if (end - start + 1 < len) {
+                    len = end - start + 1;
+                    minstart = start;
                 }
-                freq1[s[i]]++;
-
-                while (requiredChars == 0) {
-                    if (i - start + 1 < minWindowSize) {
-                        minWindowSize = i - start + 1;
-                        st = start;
-                        e = i;
-                    }
-
-                    if (freq[s[start]]) {
-                        freq1[s[start]]--;
-                        if (freq1[s[start]] < freq[s[start]]) {
-                            requiredChars++;
-                        }
-                    }
-                    start++;
+                freq[s[start]]++;
+                if(freq[s[start]]>0){
+                    required++;
                 }
+                start++;
             }
         }
-
-        if (minWindowSize == INT_MAX) {
+        if(len == INT_MAX){
             return "";
         }
-
-        return s.substr(st, minWindowSize);
+        return s.substr(minstart,len);
     }
 };
