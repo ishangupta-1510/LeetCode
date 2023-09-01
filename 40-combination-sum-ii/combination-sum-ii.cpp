61b54d5182ccc01a8b9403df471974f2ba07bb1f
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-09-01 15:30:42 +0530

    Time: 4 ms (65.46%) | Memory: 10.5 MB (76.83%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    void targetsum(vector<vector<int>> &ans, vector<int>& vec, vector<int>& c, int target, int i) {
-        if (target == 0) {
-            ans.push_back(vec);
+    void targetsum(vector<vector<int>> &v,vector<int> &vec,vector<int>& c,int target,int i){
+        if(i==c.size()){
+            if(target == 0){
+                // sort(vec.begin(),vec.end());
+                v.push_back(vec);
+            }
             return;
         }
-        if (i == c.size() || target < 0) {
-            return;
+        if(c[i]<=target){
+            vec.push_back(c[i]);
+            targetsum(v,vec,c,target-c[i],i+1);
+            vec.pop_back();
         }
-        
-        // Include the current element in the combination
-        vec.push_back(c[i]);
-        targetsum(ans, vec, c, target - c[i], i + 1);
-        vec.pop_back();
-        
-        // Skip duplicates
         while (i + 1 < c.size() && c[i] == c[i + 1]) {
             i++;
         }
-        
-        // Exclude the current element and move to the next
-        targetsum(ans, vec, c, target, i + 1);
+        targetsum(v,vec,c,target,i+1);
     }
-    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
+        vector<vector<int>> v;
         vector<int> vec;
-        
-        // Sort the candidates to handle duplicates properly
-        sort(candidates.begin(), candidates.end());
-        
-        targetsum(ans, vec, candidates, target, 0);
-        
-        return ans;
+        sort(candidates.begin(),candidates.end());
+        targetsum(v,vec,candidates,target,0);
+        return v;
     }
 };
