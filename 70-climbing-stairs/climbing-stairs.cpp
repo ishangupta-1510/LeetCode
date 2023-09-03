95e6a202951f61ed591b1012dc5f78241896ac8e
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-09-03 17:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (42.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -7,14 +7,19 @@ public:
         if(n==2){
             return 2;
         }
+        
         if(dp[n] != -1){
             return dp[n];
         }
         return dp[n] = climbStairs(n-1,dp)+climbStairs(n-2,dp);
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return climbStairs(n,dp);
+        vector<int> dp(max(n+1,3),-1);
+        dp[1] = 1,dp[2] = 2;
+        climbStairs(n,dp);
+        for(int i=0;i<n+1;i++){
+            cout<<dp[i]<<' ';
+        }
         return dp[n];
     }
 };
