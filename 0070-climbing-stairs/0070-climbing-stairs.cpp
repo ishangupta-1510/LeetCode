cfdcb50992325ba0b036c01464b71be850af3138
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-09-03 17:54:18 +0530

    Time: 3 ms (20.2%), Space: 6.2 MB (18.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int> dp(n+1,0);
-        dp[1] = 1;dp[0]=1;
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+    int climbStairs(int n,vector<int>& dp){
+        if(n==1){
+            return 1;
         }
+        if(n==2){
+            return 2;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = climbStairs(n-1,dp)+climbStairs(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return climbStairs(n,dp);
         return dp[n];
     }
 };
