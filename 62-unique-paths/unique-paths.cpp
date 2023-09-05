caee2963821759898100b729ae88b7c47a0871e3
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-09-05 13:38:44 +0530

    Time: 4 ms (11.73%) | Memory: 6.5 MB (26.96%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int countpath(int i,int j,int m,int n,vector<vector<int>>& dp){
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(i==m-1 and j==n-1){
+    int countpath(int m,int n,vector<vector<int>> &dp){
+        
+        if(m==0 and n==0){
             return 1;
         }
-        else if(i>m-1 or j>n-1){
+        else if(m<0 or n<0){
             return 0;
         }
-        return dp[i][j] = countpath(i+1,j,m,n,dp)+countpath(i,j+1,m,n,dp);
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        return dp[m][n] = countpath(m-1,n,dp)+countpath(m,n-1,dp);
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        int result = countpath(0,0,m,n,dp);
-        return result;
+        return countpath(m-1,n-1,dp);
     }
 };
