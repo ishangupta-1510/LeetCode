434a8380f24bb8831122a7c6d0789703ddb77875
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-10-11 14:17:44 +0530

    Time: 27 ms (25.45%), Space: 20.6 MB (15.93%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,32 +2,38 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.length();
-        int ans = 0;
-
-        // Odd length palindromes
-        for (int i = 0; i < n; i++) {
-            int right = i, left = i;
-            int count = 0;
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
-            }
-            ans += count;
+        int len = 0, start = 0;
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        for(int i=0;i<n;i++){
+            dp[i][i] = 1;
+            len = 1;
+            start = i;
         }
-
-        // Even length palindromes
-        for (int i = 0; i < n - 1; i++) {
-            int right = i + 1, left = i;
-            int count = 0;
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
+        for(int i=0;i<n-1;i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = 1;
+                len = 2;
+                start = i;
             }
-            ans += count;
         }
-
-        return ans;
+        
+        for(int j=2;j<n;j++){
+            for(int i=0;i<n-j+1;i++){
+                if(dp[i+1][j+i-1] and s[i] == s[j+i]){
+                    dp[i][j+i] = 1;
+                    len = j+1;
+                    start = i;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dp[i][j] == 1){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
