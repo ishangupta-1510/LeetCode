dff2232a65f4a927e50d8367120dcf2fffc5aa95
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-10-14 14:36:47 +0530

    Time: 29 ms (84.25%) | Memory: 19.5 MB (37.18%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int n = heights.size(), m = heights[0].size();
-        if (n == 0 || m == 0) {
+        if (heights.empty() || heights[0].empty()) {
             return {};  // Return an empty result for empty input.
         }
-
-        vector<vector<int>> visP(n, vector<int>(m, 0));
-        vector<vector<int>> visA(n, vector<int>(m, 0));
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<int>> visP(n,vector<int> (m,0));
+        vector<vector<int>> visA(n,vector<int> (m,0));
         vector<vector<int>> ans;
-
-        for (int i = 0; i < m; i++) {
-            dfs(heights, 0, i, visP, n, m);
-            dfs(heights, n - 1, i, visA, n, m);
+        for(int i=0;i<m;i++){
+            dfs(heights,0,i,visP,n,m);
+            dfs(heights,n-1,i,visA,n,m);
         }
-        for (int i = 0; i < n; i++) {
-            dfs(heights, i, 0, visP, n, m);
-            dfs(heights, i, m - 1, visA, n, m);
+        for(int i=0;i<n;i++){
+            dfs(heights,i,0,visP,n,m);
+            dfs(heights,i,m-1,visA,n,m);
         }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (visP[i][j] && visA[i][j]) {
-                    ans.push_back({i, j});
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visP[i][j] and visA[i][j]){
+                    ans.push_back({i,j});
                 }
             }
         }
         return ans;
     }
-
-    void dfs(vector<vector<int>>& heights, int i, int j, vector<vector<int>>& vis, int n, int m) {
-        int crow[] = {0, 0, -1, 1};
-        int ccol[] = {-1, 1, 0, 0};
+    void dfs(vector<vector<int>>& heights,int i,int j,vector<vector<int>>& vis,int n,int m){
+        int crow[] = {0,0,-1,1};
+        int ccol[] = {-1,1,0,0};
         vis[i][j] = 1;
-        
-        for (int k = 0; k < 4; k++) {
+        for(int k=0;k<4;k++){
             int nrow = i + crow[k];
             int ncol = j + ccol[k];
-            
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && heights[i][j] <= heights[nrow][ncol]) {
-                dfs(heights, nrow, ncol, vis, n, m);
+            if(nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and !vis[nrow][ncol] and heights[i][j] <= heights[nrow][ncol]){
+                dfs(heights,nrow,ncol,vis,n,m);
             }
         }
     }
