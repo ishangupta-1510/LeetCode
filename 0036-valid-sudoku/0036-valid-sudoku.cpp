06d9ce32a7197d01f04aef286a480edfd455df3e
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-10-23 14:24:00 +0530

    Time: 18 ms (59.23%), Space: 20.2 MB (37.63%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+        bool isValidSudoku(vector<vector<char>>& board) {
+        map<char,int> row[9],col[9],blk[9];
+        char c;
+        for(int i = 0;i<9;i++){
+            for(int j =0;j<9;j++){
+                c = board[i][j];
+                if((c!='.')&&(0<row[i][c]++ || 0<col[j][c]++ || 0<blk[i/3*3+j/3][c]++)) return false;
+            }
+        }
+        return true;
+    }
+};
