0e939196df00a0260d53cbd672097f464fc22e09
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-10-23 14:24:00 +0530

    Time: 18 ms (59.23%) | Memory: 20.2 MB (37.63%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9][9]={0}; 
-	    int cols[9][9]={0}; 
-	    int blocks[3][3][9]={0};
-        for(int r=0;r<9;r++) 
-            for(int c=0;c<9;c++)
-                if(board[r][c]!='.'){
-                    int number=board[r][c]-'1';
-                    if(rows[r][number]++) return false;
-                    if(cols[c][number]++) return false;
-                    if(blocks[r/3][c/3][number]++) return false;
-                }
-        return true;
+        bool isValidSudoku(vector<vector<char>>& board) {
+        map<char,int> row[9],col[9],blk[9];
+        char c;
+        for(int i = 0;i<9;i++){
+            for(int j =0;j<9;j++){
+                c = board[i][j];
+                if((c!='.')&&(0<row[i][c]++ || 0<col[j][c]++ || 0<blk[i/3*3+j/3][c]++)) return false;
+            }
+        }
+        return true;
     }
 };
