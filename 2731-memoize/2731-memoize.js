a754634dce88e90604e811497533403b069ffa17
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2023-11-03 12:33:00 +0530

    Time: 330 ms (17.59%), Space: 106.8 MB (84.44%) - LeetHub

---
+++ 2731-memoize/2731-memoize.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if(args in cache){
+            return cache[args];
+        }
+        const result = fn(...args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
