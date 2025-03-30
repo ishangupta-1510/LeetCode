b631dfa153f0e6faca7a0b2544a7beca8fe0fcf7
ishangupta-1510 <75131633+ishangupta-1510@users.noreply.github.com>
2025-03-30 20:08:07 +0530

    Time: 0 ms (100%), Space: 57.7 MB (50.49%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0, end = nums.length - 1;
+    while (start<=end){
+        let mid = Math.floor((start+end)/2);
+        if(nums[mid] <= target){
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    return nums[end] === target ? end:-1;
+};
